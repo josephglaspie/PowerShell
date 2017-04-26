3e1deef82bb43a8196088e32577b667479ae9d7f
Joseph Glaspie <josephglaspie@gmail.com>
2017-04-25 20:22:22 -0500

    Py stuff

--- PycharmProjects/Py3/countCharacters.py
+++ PycharmProjects/Py3/countCharacters.py
@@ -1,3 +1,4 @@
+import pprint
 message = 'It was a bright cold day in April, and the clocks were striking thirteen.'
 count = {}
 
@@ -5,4 +6,5 @@ for character in message:
     count.setdefault(character, 0)
     count[character] = count[character] + 1
 
-print(count)
+pprint.pprint(count)
+print("Total chars: " + str(sum(count.values())))
