5be342f0565bbc65082609f9e111d64301214f52
Joseph <josephglaspie@gmail.com>
2017-05-23 17:40:58 -0500

    Create Post-ReloadChocoInstalls.ps1

---
+++ Post-ReloadChocoInstalls.ps1
@@ -0,0 +1,9 @@
+#region Install apps
+Write-Host "Installing Chocolatey" -ForegroundColor Green
+iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+
+Write-Host "Installing Apps" -ForegroundColor Green
+choco install 7zip git greenshot node beyondcompare slack atom notepadplusplus visualstudiocode nodejs sql-server-management-studio keepass rdcman rsat expresso adobereader pycharm-community consolez python vlc -y
+#endregion
+
+ 
