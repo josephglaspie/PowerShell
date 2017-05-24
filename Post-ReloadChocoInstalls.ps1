0f5bd61e1219f1ad3664ca022962ac0414f3320f
Joseph <josephglaspie@gmail.com>
2017-05-24 05:32:03 -0500

    Update Post-ReloadChocoInstalls.ps1

--- Post-ReloadChocoInstalls.ps1
+++ Post-ReloadChocoInstalls.ps1
@@ -1,11 +1,11 @@
 #region Install apps
-$Original=Get-ExecutionPolicy
-Set-executionpolicy bypass
+$o=Get-ExecutionPolicy
+Set-ExecutionPolicy Bypass -Force
+
 Write-Host "Installing Chocolatey" -ForegroundColor Green
 iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 
 Write-Host "Installing Apps" -ForegroundColor Green
 choco install 7zip git greenshot node beyondcompare slack atom notepadplusplus visualstudiocode nodejs sql-server-management-studio keepass rdcman rsat expresso adobereader pycharm-community consolez python vlc -y
 #endregion
-Set-ExecutionPolicy $Original
- 
+Set-ExecutionPolicy $o -Force
