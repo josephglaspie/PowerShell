5702581ee13de9bd620a55bd3e02c7699491bc98
JXG2980 <joseph_glaspie@homedepot.com>
2017-09-01 08:57:47 -0500

    removed sendGrid api

--- nodemailer/index.js
+++ nodemailer/index.js
@@ -1,7 +1,7 @@
 //set SENDGRID_API_KEY=''
 //SENDGRID_API_KEY='https://app.sendgrid.com/guide/integrate/langs/smtp'
 //
-var SENDGRID_API_KEY = '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';
+var SENDGRID_API_KEY = ''
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(SENDGRID_API_KEY);
 const msg = {
