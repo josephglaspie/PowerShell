38613aa35d0a6d4ab5ea5043d307f0879952c7af
JXG2980 <joseph_glaspie@homedepot.com>
2017-10-04 11:59:53 -0500

    added testmail

--- forms2POST/routebasics/app.js
+++ forms2POST/routebasics/app.js
@@ -1,6 +1,6 @@
 //This app will load .env info like secrect keys from the .env File. You can 
 //also fill out forms here, good template for contact us forms
-
+var sgMail = require('@sendgrid/mail');
 var http = require('http');
 var express = require('express');
 var app = express();
@@ -9,6 +9,8 @@ dotenv.load();
 var bodyParser = require('body-parser'); //for parsing the body req.body.WHATEVER
 app.use(bodyParser.urlencoded({ extended: false })); //for parsing the body req.body.WHATEVER
 app.use(bodyParser.json()); //for parsing the body req.body.WHATEVER
+var SECRET_KEY = process.env.SECRET_KEY;
+sgMail.setApiKey(SECRET_KEY);
 
 var port = process.env.port || 3000;
 
@@ -23,6 +25,25 @@ app.post("/", function(req, res) {
     var password = req.body.password;
     res.send("the username and password u posted are: " + username + " & " + password);
 });
+
+app.get('/contact', function(req, res) {
+    res.sendfile('./contact.html');
+});
+
+app.post("/contact", function(req, res) {
+
+    var msg = {
+        to: 'josephglaspie@gmail.com',
+        from: req.body.from,
+        subject: req.body.subject,
+        text: req.body.message,
+        //html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+
+    };
+    sgMail.send(msg).then().catch();
+    res.end("We'll get back with you ASAP!");
+});
+
 
 app.get("/form", function(req, res) {
     res.sendfile("./form.html"); //sending file to client browser
