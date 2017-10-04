286db4fe0580a4c53d4dbe987f035edf8980b880
Joseph <josephglaspie@gmail.com>
2017-10-04 12:02:45 -0500

    removed sendgrid api

--- testMail/app.js
+++ testMail/app.js
@@ -1,5 +1,5 @@
 //NOT WORKING
-var sendgrid = require("sendgrid")("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");
+var sendgrid = require("sendgrid")("APIKey");
 var email = new sendgrid.Email();
 
 email.addTo("josephglaspie@gmail.com");
